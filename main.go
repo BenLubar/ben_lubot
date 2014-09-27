d069fd4f7f23a4df6b7ea7d7b13dc6e8adefdf70
Ben Lubar <ben.lubar@gmail.com>
2014-09-27 11:25:44 -0500

    initial commit

---
+++ main.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+	"code.google.com/p/go.net/publicsuffix"
+	"encoding/json"
+	"log"
+	"math/rand"
+	"net/http"
+	"net/http/cookiejar"
+	"net/url"
+	"strconv"
+	"sync"
+	"time"
+)
+
+var Client http.Client
+var MyID int
+
+func init() {
+	blt := &ben_lubot_transport{base: http.DefaultTransport}
+	Client.Transport = blt
+	jar, err := cookiejar.New(&cookiejar.Options{PublicSuffixList: publicsuffix.List})
+	if err != nil {
+		panic(err)
+	}
+	Client.Jar = jar
+
+	v, err := GetJSON("/session/csrf")
+	if err != nil {
+		panic(err)
+	}
+	blt.csrf = v["csrf"].(string)
+}
+
+type ben_lubot_transport struct {
+	base http.RoundTripper
+	csrf string
+	wait sync.Mutex
+}
+
+func (blt *ben_lubot_transport) RoundTrip(req *http.Request) (*http.Response, error) {
+	if req.Method != "POST" {
+		q := req.URL.Query()
+		q.Set("_", strconv.FormatInt(time.Now().UnixNano()/int64(time.Millisecond), 10))
+		req.URL.RawQuery = q.Encode()
+	}
+	req.Header.Set("X-Requested-With", "XMLHttpRequest")
+	if blt.csrf != "" {
+		req.Header.Set("X-CSRF-Token", blt.csrf)
+	}
+
+	blt.wait.Lock()
+	time.Sleep(time.Second)
+	blt.wait.Unlock()
+
+	log.Println(req)
+
+	return blt.base.RoundTrip(req)
+}
+
+func GetJSON(path string) (v map[string]interface{}, err error) {
+	resp, err := Client.Get(Host + path)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	err = json.NewDecoder(resp.Body).Decode(&v)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func PostJSON(path string, data url.Values) (v map[string]interface{}, err error) {
+	resp, err := Client.PostForm(Host+path, data)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	err = json.NewDecoder(resp.Body).Decode(&v)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func PostDiscard(path string, data url.Values) (err error) {
+	resp, err := Client.PostForm(Host+path, data)
+	if err != nil {
+		return
+	}
+
+	err = resp.Body.Close()
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func ReadTopic(id int) {
+	s_id := strconv.Itoa(id)
+
+	topic, err := GetJSON("/t/" + s_id + ".json")
+	if err != nil {
+		panic(err)
+	}
+
+	n := strconv.Itoa(1000*60*3 + rand.Intn(100))
+	v := url.Values{
+		"topic_time": {n},
+		"topic_id":   {s_id},
+	}
+	for _, p := range topic["post_stream"].(map[string]interface{})["posts"].([]interface{}) {
+		read, _ := p.(map[string]interface{})["read"].(bool)
+		if !read {
+			v["timings["+strconv.Itoa(int(p.(map[string]interface{})["post_number"].(float64)))+"]"] = []string{n}
+		}
+	}
+
+	err = PostDiscard("/topics/timings", v)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func BackfillLatest() {
+	url := "/latest.json"
+	for {
+		latest, err := GetJSON(url)
+		if err != nil {
+			panic(err)
+		}
+
+		tl := latest["topic_list"].(map[string]interface{})
+
+		for _, t := range tl["topics"].([]interface{}) {
+			ReadTopic(int(t.(map[string]interface{})["id"].(float64)))
+		}
+
+		var ok bool
+		url, ok = tl["more_topics_url"].(string)
+		if !ok {
+			return
+		}
+	}
+}
+
+func Authenticate() {
+	v, err := PostJSON("/session", url.Values{"login": {Username}, "password": {Password}})
+	if err != nil {
+		panic(err)
+	}
+
+	MyID = int(v["user"].(map[string]interface{})["id"].(float64))
+}
+
+func main() {
+	Authenticate()
+	BackfillLatest()
+}
